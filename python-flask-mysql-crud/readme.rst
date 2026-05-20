5256401b19457bef4c3db9d37fd64d90f415f8ee
Soumitra <projwebinfo@gmail.com>
2026-05-20 19:49:43 +0530

    Delete python-flask-mysql-crud directory

--- python-flask-mysql-crud/readme.rst
+++
@@ -1,1 +0,0 @@
-Please follow the tutorial https://roytuts.com/python-web-application-crud-example-using-flask-and-mysql/
--- python-flask-mysql-crud/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tbl_user` (
-  `user_id` bigint NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(45) DEFAULT NULL,
-  `user_email` varchar(45) DEFAULT NULL,
-  `user_password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
